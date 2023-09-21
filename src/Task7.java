c1f79340cd17d023a25ab96114f991818280247a
StGrey <vt211_nvs@student.ztu.edu.ua>
2023-09-21 22:41:13 +0300

    Completed task 7

--- src/Task7.java
+++ src/Task7.java
@@ -1,5 +1,14 @@
 public class Task7 {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Task 7");
+    public static void main(String[] args) {
+        char[] alphabet = new char[26];
+
+        for (int i = 0; i < 26; i++) {
+            alphabet[i] = (char) ('A' + i);
+        }
+
+        for (char letter : alphabet) {
+            int charCode = (int) letter;
+            System.out.println(letter + " ==> " + charCode);
+        }
     }
 }
